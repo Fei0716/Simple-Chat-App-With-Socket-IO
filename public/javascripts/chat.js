84ebf2dc4c93ea2f2b0825c2f878fd93117998cb
Fei0716 <ahfeiahfeiahfei@gmail.com>
2024-08-27 20:01:08 +0800

    Added private one-to-one messaging and room messaging

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,22 +1,37 @@
 // dom elements
-const btnSendAllDom = document.querySelector('#btn-send-all');
+const btnSendDom = document.querySelector('#btn-send');
 const inputDom = document.querySelector('#input');
 const chatOutputDom = document.querySelector('#chat-output');
 const onlineUsersDom = document.querySelector('#online-users');
+const roomDom = document.querySelector('#room');
+const btnJoinDom = document.querySelector('#btn-join');
 
 //states
 let users = [];
-
+let room;
 //create instance of socket io
 //socket configs
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:3000/chat", {
+    //passing token here for authentication
+    auth:{
+        token: "auth_token_here"
+    }
+});//we are using the socket with the namespace of chat
 socket.on('connect', ()=>{
-    console.log('connected to socket io');
+    console.log('connected to socket io', socket);
 });
 //when there's new message from any other users connected to the socket
-socket.on("receiveMessage", (message) => {
+socket.on("receiveMessage", (message , sender) => {
     chatOutputDom.innerHTML += `
-        <div>${message}</div>
+    <div class="chat"> 
+        <h3>
+            ${sender}
+        </h3>
+        <p>
+            ${message}
+        </p> 
+        
+    </div>
     `
 });
 //get the list of current online users when first time connected to the socket
@@ -25,12 +40,38 @@ socket.on("getSocketIds", (ids) =>{
     //reset dom
     onlineUsersDom.innerHTML = '<h2>List of Online Users</h2>';
     for(let u of users){
-        //append to list of online users dom
-        onlineUsersDom.innerHTML += `
-        <div class="user-socket" data-socket-id="${u}">
-             ${u}
-        </div>
-    `
+        let userSocketDiv = document.createElement('div');
+        userSocketDiv.classList.add('user-socket');
+        userSocketDiv.setAttribute('data-socket-id', u);
+        userSocketDiv.textContent = u;
+        if(u === socket.id){
+            userSocketDiv.classList.add('current-user');
+            userSocketDiv.textContent = u + ' (You)' ;
+            onlineUsersDom.append(userSocketDiv);
+        }else{
+            userSocketDiv.textContent = u ;
+            onlineUsersDom.append(userSocketDiv);
+        }
+
+        //add click event to each of the user-socket
+            userSocketDiv.addEventListener("click", (e) =>{
+                //if chatting clicking on the current user
+                if(e.target.getAttribute('data-socket-id') === socket.id){
+                    return;
+                }
+                //remove selected class from the other .user-socket
+                let selectedUserSocket = document.querySelector(".user-socket.selected");
+                if(selectedUserSocket){
+                    selectedUserSocket.classList.remove('selected');
+                    if(selectedUserSocket.getAttribute('data-socket-id') === e.target.getAttribute('data-socket-id')){
+                        //if clicked on a user socket that already have .selected, remove it
+                        e.target.classList.remove('selected');
+                        return;
+                    }
+                }
+
+                e.target.classList.add('selected');
+            });
     }
 });
 
@@ -38,13 +79,36 @@ socket.on("getSocketIds", (ids) =>{
 socket.on("newUserJoined", (newUserSocketId) =>{
     //append to the online users list
     users.push(newUserSocketId);
+    let userSocketDiv = document.createElement('div');
+    userSocketDiv.classList.add('user-socket');
+    userSocketDiv.setAttribute('data-socket-id', newUserSocketId);
+    userSocketDiv.textContent = newUserSocketId;
 
     //update list dom
-    onlineUsersDom.innerHTML += `
-        <div class="user-socket" data-socket-id="${newUserSocketId}">
-             ${newUserSocketId}
-        </div>
-    `
+    onlineUsersDom.append(userSocketDiv) ;
+    //add event listener to the new .user-socket
+    userSocketDiv.addEventListener("click", (e) =>{
+        //if chatting clicking on the current user
+        if(e.target.getAttribute('data-socket-id') === socket.id){
+            return;
+        }
+
+
+        //remove selected class from the other .user-socket
+        let selectedUserSocket = document.querySelector(".user-socket.selected");
+        if(selectedUserSocket){
+            selectedUserSocket.classList.remove('selected');
+
+            if(selectedUserSocket.getAttribute('data-socket-id') === e.target.getAttribute('data-socket-id')){
+                //if clicked on a user socket that already have .selected, remove it
+                e.target.classList.remove('selected');
+                return;
+            }
+        }
+
+        e.target.classList.add('selected');
+    });
+
 });
 
 //when there's a user disconnected
@@ -57,21 +121,56 @@ socket.on("userDisconnected", (id) =>{
 });
 
 // add event listeners
-btnSendAllDom.addEventListener('click', function(){
-    if(inputDom.value !== ''){
+btnSendDom.addEventListener('click', function(){
+    if(inputDom.value === '')return;
+    let selectedUserSocket = document.querySelector('.selected');
+
+    if(room ){
+        //if joined a room send to the users in the room
+        socket.emit('sendMessage' , {
+            message: inputDom.value,
+        }, null ,room);
+    }
+    else if(selectedUserSocket){
+        //if there's a selected socket send to the user
+        socket.emit('sendMessage' , {
+            message: inputDom.value,
+        }, selectedUserSocket.getAttribute('data-socket-id'));
+    }
+    else{
+        //else send to all online users
         socket.emit('sendMessage' , {
             message: inputDom.value,
         });
-
-        //append the message to dom(sender)
-        chatOutputDom.innerHTML += `
-            <div>${inputDom.value}</div>
+    }
+    //append the message to dom(sender)
+    chatOutputDom.innerHTML += `
+            <div class="chat chat-current-user">
+                <h3 class="text-end">You</h3>
+                <p class="text-end">
+                ${inputDom.value}
+                </p>
+            </div>
         `
 
-        //clear the input
-        inputDom.value = '';
-    }
+    //clear the input
+    inputDom.value = '';
 });
 
+btnJoinDom.addEventListener('click', function(){
+    if(roomDom.value === '')return;
+
+    //emit join room event back to the server to join a roow
+    socket.emit("joinRoom", roomDom.value, (message)=>{
+        //callback from the server
+        chatOutputDom.innerHTML += `
+            <div class="notification">
+                ${message}
+            </div>     
+        `;
+    });
+    room = roomDom.value;
+    roomDom.value = '';
+});
 
 
--- socket.js
+++ socket.js
@@ -7,7 +7,17 @@ module.exports =  function(server){
             origin: ['*'],
         }
     });
-    io.on("connection", socket => {
+    //create a custom namespace
+    const chatIo = io.of("/chat");
+
+    //adding middleware to check for auth token
+    chatIo.use((socket, next) =>{
+        console.log('authToken: '+ socket.handshake.auth.token);
+        next();
+    });
+
+
+    chatIo.on("connection", socket => {
         //emit notify all users that a new user has join the socket connection
         //send list of socketIds to the new user
         socketIds.push(socket.id);
@@ -16,13 +26,28 @@ module.exports =  function(server){
 
         socket.broadcast.emit("newUserJoined", socket.id);
 
-        socket.on("sendMessage" , (data)=>{
+        socket.on("sendMessage" , (data , user , room)=>{
             //send to all users including the sender
             //io.emit("receiveMessage", data.message);
 
+            //to send private message to a specific user
+            if(user){
+                socket.to(user).emit("receiveMessage", data.message, socket.id);
+                return;
+            }
+            if(room){
+                socket.to(room).emit("receiveMessage", data.message, socket.id);
+                return;
+            }
             //broadcast the message to all users except the sender
-            socket.broadcast.emit("receiveMessage", data.message);
+            socket.broadcast.emit("receiveMessage", data.message , socket.id);
+        });
 
+        socket.on("joinRoom", (room, cb)=>{
+            //join a specific room
+            socket.join(room);
+            //run the callback function in the client to send back a message
+            cb(`Joined Room: ${room}`);
         });
 
         //when the user disconnected remove his socket id
@@ -31,7 +56,8 @@ module.exports =  function(server){
             //remove from the array of socket ids
             socketIds.splice(socketIds.findIndex(s => s === socket.id) , 1);
             //broadcast event to notify the users about disconnection of the user
-            io.emit("userDisconnected" , socket.id);
+            chatIo.emit("userDisconnected" , socket.id);
         });
     })
+
 };
